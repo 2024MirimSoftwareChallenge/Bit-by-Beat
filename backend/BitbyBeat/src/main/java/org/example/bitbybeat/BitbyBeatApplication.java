dd7300d07c6e357c888114414adffde39b959be0
dayul <dyulchoo@naver.com>
2024-10-17 11:12:02 +0900

    🎉[chore] 프로젝트 시작

---
+++ backend/BitbyBeat/src/main/java/org/example/bitbybeat/BitbyBeatApplication.java
@@ -0,0 +1,13 @@
+package org.example.bitbybeat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BitbyBeatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BitbyBeatApplication.class, args);
+    }
+
+}
